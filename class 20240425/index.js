f338d8a8afd774ac6b31a05a06d0e664170b39f4
ImranAli6764 <160871518+ImranAli6764@users.noreply.github.com>
2024-04-26 11:23:10 -0700

    Add files via upload

---
+++ class 20240425/index.js
@@ -0,0 +1,30 @@
+//function meal(choice:string = "chicken"){ console.log(choice)}
+//meal()
+//meal("korma")
+//function Day(name:string){
+//return name
+//}
+//console.log(Day("thursday"));
+//function Name():string{return "imranromail"}
+//let firstName = Name()
+//console.log(firstName);
+//function bool():boolean {return true}
+//let ans = bool()
+//console.log(ans)
+//function multiply():number{return 5*10}
+//let ans = multiply()
+//console.log(ans);
+//practice
+//function GetCard(timing:string , rollno:number){console.log(`${timing} ${rollno}`)}
+//GetCard("7-10pm" , 16793)
+//GetCard("6-09pm" , 88765)
+//GetCard("9-5pm" , 12345)
+//DEFAULT PARAMETERS:
+//function Marriage(choice:string = "ammi ki choice"){console.log(choice);}
+//Marriage()
+//Marriage("meri choice")
+//function add(d:number = 5 , dtwo:number = 6 ){console.log(d + dtwo);}
+//add(undefined , 10)
+//return name:
+function Day(name) { return name; }
+console.log(Day("thursday"));
